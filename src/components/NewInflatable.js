7acb2194a6c1e4113c474c7705ac1521690a2b48
dieg0espx <espinosa9mx@gmail.com>
2024-02-01 14:09:03 -0800

    .

--- src/components/NewInflatable.js
+++ src/components/NewInflatable.js
@@ -45,32 +45,29 @@ function NewInflatable() {
     <div className='popup-newInflatable'>
         <h2> Add New Inflatable </h2>
         <form onSubmit={createNewInflatable}>
-            <div className='cols'>
-                <input type="text" name="name" value={newInflatable.name} onChange={handleInputChange} placeholder='Name'/>
-                <input type="number" name="price" value={newInflatable.price} onChange={handleInputChange} placeholder='Price'/>
-            </div>
+        <input type="text" name="name" value={newInflatable.name} onChange={handleInputChange} placeholder='Inflatable Name'/>
             <textarea type="text" name="description" value={newInflatable.description} onChange={handleInputChange} placeholder='Description'/>
-            <select name='category' value={newInflatable.category} onChange={handleInputChange}>
-                <option selected> Select a category</option>
-                <option value={'bounce houses'}> Bounce Houses </option>
-                <option value={'combo jumpers'}> Combo Jumpers </option>
-                <option value={'slides'}> Slides </option>
-                <option value={'games and obstacles'}> Games & Obstacles </option>
-                <option value={'extras'}> Extras </option>
-            </select>
+            <div className='cols'>
+                <select name='category' value={newInflatable.category} onChange={handleInputChange}>
+                    <option value={'bounce houses'}> Bounce Houses </option>
+                    <option value={'combo jumpers'}> Combo Jumpers </option>
+                    <option value={'slides'}> Slides </option>
+                    <option value={'games and obstacles'}> Games & Obstacles </option>
+                    <option value={'extras'}> Extras </option>
+                </select>
+                <p id="dollarSign">$</p>
+                <input id="price" type="number" name="price" value={newInflatable.price} onChange={handleInputChange} placeholder='Price'/>
+                <p id="currency"> USD</p>
+            </div>
             <div className='cols'>
                 <input type="text" name="width" value={newInflatable.width} onChange={handleInputChange} placeholder='Width'/>
                 <input type="number" name="height" value={newInflatable.height} onChange={handleInputChange} placeholder='Height'/>
                 <select name='wetDry' value={newInflatable.wetDry} onChange={handleInputChange}>
-                    <option selected> Select an Option  </option>
-                    <option> Dry </option>
-                    <option> Wet </option>
+                    <option value={'Dry'}> Dry </option>
+                    <option value={'Wet'}> Wet </option>
                 </select>
             </div>   
-            <div className='cols'>
-                <input type="text" name="image" value={newInflatable.image} onChange={handleInputChange} placeholder='Image URL'/>
-            </div>         
-            
+            <input type="text" name="image" value={newInflatable.image} onChange={handleInputChange} placeholder='Image URL'/>     
             <button type='submit'> Add New Inflatable </button>
         </form>
     </div>
