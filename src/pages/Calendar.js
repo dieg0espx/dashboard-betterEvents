dc42282bef3d705879afedb6d80cc9dc93bae25d
dieg0espx <espinosa9mx@gmail.com>
2024-02-05 15:29:11 -0800

    .

--- src/pages/Calendar.js
+++ src/pages/Calendar.js
@@ -11,15 +11,15 @@ function Calendar() {
     const db = getFirestore(app)
     const [events, setEvents] = useState([])
     const [bookings, setBookings] = useState([])
+    const [currentBooking, setCurrentBooking] = useState([])
+    const [showCurrentBooking, setShowCurrentBooking]= useState(false)
 
-
-
-  async function getBookings(){
-    const querySnapshot = await getDocs(collection(db, 'bookings'));
-    let arrayBookings = [];
-    let arrayEvents = []
-  
-    querySnapshot.docs.map(async (doc) => {
+    async function getBookings(){
+      const querySnapshot = await getDocs(collection(db, 'bookings'));
+      let arrayBookings = [];
+      let arrayEvents = []
+    
+      querySnapshot.docs.map(async (doc) => {
       arrayBookings.push({
         id: doc.id,
         address: doc.data().address,
@@ -36,18 +36,24 @@ function Calendar() {
         title:doc.data().name + ' ' + doc.data().lastName, 
         start: new Date((doc.data().bookingDates[0])),
         end: new Date(new Date(doc.data().bookingDates[doc.data().bookingDates.length - 1]).setDate(new Date(doc.data().bookingDates[doc.data().bookingDates.length - 1]).getDate() + 1)),
+        id:doc.id,
         backgroundColor:getRandomColor()
       })
-    })
-
-    setEvents(arrayEvents)
-    setBookings(arrayBookings);
-  }
+      })
+      setEvents(arrayEvents)
+      setBookings(arrayBookings);
+    }
 
     useEffect(()=>{
         getBookings()
     },[])
-
+    useEffect(() => {
+      if(showCurrentBooking){
+        document.body.style.overflow = 'hidden';
+      } else {
+        document.body.style.overflow = 'visible';
+      }
+    }, [showCurrentBooking]);
     function getRandomColor() {
       const getRandomComponent = () => Math.floor(Math.random() * 128 + 64).toString(16).padStart(2, '0');
       return `#${getRandomComponent()}${getRandomComponent()}${getRandomComponent()}`;
@@ -55,7 +61,7 @@ function Calendar() {
 
     const eventContent = (eventInfo) => {
       return (
-        <div className="custom-event" style={{backgroundColor: getRandomColor()}}>
+        <div className="custom-event">
           <div className="event-title">{eventInfo.event.title}</div>
           <div className="event-time">
             {formatDate(eventInfo.event.start)} - {formatDate(eventInfo.event.end.setDate(eventInfo.event.end.getDate() - 1))}
@@ -70,9 +76,25 @@ function Calendar() {
       return date.toLocaleDateString(undefined, options)
     };
 
+    function handleEventClick(eventClickInfo) {
+      const { event } = eventClickInfo;
+      getCurrentBooking(event.id)
+      setShowCurrentBooking(true)
+    }
+    function getCurrentBooking(id){
+      for(let i =0; i < bookings.length; i ++){
+        if(bookings[i].id == id){
+          setCurrentBooking(bookings[i])
+        }
+      }
+    }
 
-
-
+    function strToDate(str){
+      const dateObject = new Date(str);
+      const options = {weekday:'short', year: 'numeric', month: 'long', day: 'numeric' };
+      const formattedDate = dateObject.toLocaleDateString('en-US', options);
+      return formattedDate
+   }
 
   return (
     <div className='calendar-page'>
@@ -80,13 +102,47 @@ function Calendar() {
             <Sidebar />    
         </div>
         <div className='content'>
-        <FullCalendar
-        plugins={[dayGridPlugin]}
-        initialView='dayGridMonth'
-        // weekends={false}
-        events={events}
-        eventContent={eventContent}
-      />
+          <FullCalendar plugins={[dayGridPlugin]} initialView='dayGridMonth' eventClick={handleEventClick} events={events} eventContent={eventContent} />
+          <div className='booking-popup' style={{display: showCurrentBooking? "block":"none"}}>
+            <img src={currentBooking.inflatableImage} />
+            <div className='field'>
+              <p className='label'> Inflatable </p>
+              <p className='value'> Inflatable Name </p>
+            </div>
+            <div className='field'>
+              <p className='label'> Booking Id </p>
+              <p className='value'> {currentBooking.id} </p>
+            </div>
+            <div className='field'>
+              <p className='label'> Customer Name </p>
+              <p className='value'> {currentBooking.name} {currentBooking.lastName} </p>
+            </div>
+            <div className='field'>
+              <p className='label'> Customer Phone </p>
+              <p className='value'> {currentBooking.phone} </p>
+            </div>
+            <div className='field'>
+              <p className='label'> Customer Email </p>
+              <p className='value'> {currentBooking.email} </p>
+            </div>
+            <div className='field'>
+              <p className='label'> Delivery Address </p>
+              <p className='value'> {currentBooking.address} {currentBooking.postalCode} </p>
+            </div>
+
+            <div className='field'>
+              <p className='label'> Booking Dates </p>
+              {currentBooking.bookingDates?.map((date, index) => (
+                <React.Fragment key={index}>                  
+                  {strToDate(date)}
+                  <br></br>
+                  <hr></hr>
+                </React.Fragment>
+              ))}
+            </div>
+
+          </div>
+          <div className='overlay' onClick={()=>setShowCurrentBooking(!showCurrentBooking)} style={{display: showCurrentBooking? "block":"none"}}></div>
         </div>
     </div>
   )
